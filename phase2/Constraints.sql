0fb7520585f0581566bc4313c63d4ba7787c0809
ChayaGayer <116886451+ChayaGayer@users.noreply.github.com>
2025-04-07 16:57:20 +0300

    Update Constraints.sql

--- phase2/Constraints.sql
+++ phase2/Constraints.sql
@@ -7,6 +7,6 @@ ALTER COLUMN ReviewComment SET NOT NULL;
 ALTER TABLE Orders
 ADD CONSTRAINT chk_total_amount_positive CHECK (TotalAmount > 0);
 
--- Add DEFAULT value to Dishes.Category
+-- Add DEFAULT value to Delivers.DeliveryStatus
 ALTER TABLE Delivers
 ALTER COLUMN DeliveryStatus SET DEFAULT status1;
